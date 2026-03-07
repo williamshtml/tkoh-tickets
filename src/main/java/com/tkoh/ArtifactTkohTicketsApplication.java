eeb7dd447c31f6e85fc08841ec3b41fe202abc88
Williams <willixms2004@gmail.com>
2026-03-06 20:02:34 -0500

    Nada Nuevo Agregado por ahora

---
+++ src/main/java/com/tkoh/ArtifactTkohTicketsApplication.java
@@ -0,0 +1,13 @@
+package com.tkoh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtifactTkohTicketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtifactTkohTicketsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tkoh/ArtifactTkohTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tkoh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtifactTkohTicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
